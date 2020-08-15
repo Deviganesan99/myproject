b1ccdb0b4b14219912f7aba464f70d64dc7a9d40
Deviganesan99 <devi25ganesan@gmail.com>
2020-08-15 08:22:03 +0530

    Quar on time

--- springboot2-mssql-jpa-hibernate-crud-example/springboot2-mssql-jpa-hibernate-crud-example/src/main/java/net/guides/springboot2/crud/controller/ItemController.java
+++ springboot2-mssql-jpa-hibernate-crud-example/springboot2-mssql-jpa-hibernate-crud-example/src/main/java/net/guides/springboot2/crud/controller/ItemController.java
@@ -9,18 +9,12 @@ import java.util.Map;
 import net.guides.springboot2.crud.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import net.guides.springboot2.crud.exception.ResourceNotFoundException;
 import net.guides.springboot2.crud.model.Item;
 
+@CrossOrigin(origins="http://localhost:3000",maxAge=3600)
 @RestController
 @RequestMapping("/api/v1")
 public class ItemController {
